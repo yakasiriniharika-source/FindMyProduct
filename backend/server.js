274074a2b8a2807d50b3d3c3f5359b949810b3d1
niha <niharikayakasiri@gmail.com>
2026-04-05 21:57:08 +0530

    removed frontend build

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,10 @@ app.use("/api/stores", storeRoutes);
 app.use("/api/owner", ownerRoutes);
 
 // Serve React build in production
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
